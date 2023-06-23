e758530f35659aa6e8738cd78581d9e445c3c726
michaelmirhom <micharby73@gmail.com>
2023-06-23 15:52:13 -0500

    Add routing in App component

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MoviesList from './components/MoviesList';
+import MovieDetails from './components/MovieDetails';
+import NewMovieForm from './components/NewMovieForm';
+import EditMovieForm from './components/ EditMovieForm';
+import './styles/project-styles.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/movies')
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const addMovie = (newMovie) => {
+    setMovies([...movies, newMovie]);
+  };
+
+  const removeMovie = (id) => {
+    const updatedMovies = movies.filter(movie => movie.id !== id);
+    setMovies(updatedMovies);
+    // Here, you should also send a DELETE request to your backend server 
+    // to actually delete the movie from the database.
+  };
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<MoviesList movies={movies} removeMovie={removeMovie} />} />
+        <Route path="/movies" element={<MoviesList movies={movies} removeMovie={removeMovie} />} />
+        <Route path="/movies/new" element={<NewMovieForm addMovie={addMovie} />} />
+        <Route path="/movies/:id" element={<MovieDetails movies={movies} />} />
+        <Route path="/movies/:id/edit" element={<EditMovieForm movies={movies} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
