1030247e081c7e836619e94aa4b8b3dea7eb1949
michaelmirhom <micharby73@gmail.com>
2023-06-23 18:06:16 -0500

    Add function MovieDetail

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -2,3 +2,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import '../styles/project-styles.css';
+
+function MovieDetails({ movies }) {
+  const { id } = useParams();
+  const movie = movies.find(m => m.id === parseInt(id));
+
+  if (!movie) return <h3>Loading...</h3>;
+
+  return (
+    <div className="movie-card container">
+      <h2>{movie.name}</h2>
+      <img className="movie-image" src={movie.image} alt={movie.name} />
+      <p>Rating: {movie.rating}</p>
+    </div>
+  );
+}
+
+export default MovieDetails;
