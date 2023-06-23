02e2de429e7224937cdacf08e54d826f57147946
michaelmirhom <micharby73@gmail.com>
2023-06-23 15:47:04 -0500

    Add import files in my App file

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MoviesList from './components/MoviesList';
+import MovieDetails from './components/MovieDetails';
+import NewMovieForm from './components/NewMovieForm';
+import EditMovieForm from './components/ EditMovieForm';
+import './styles/project-styles.css';
