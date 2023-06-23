9438ea3bd3048e90aca16fd3d0bc8685ef039925
michaelmirhom <micharby73@gmail.com>
2023-06-23 15:44:32 -0500

    add NewMovieForm Code

---
+++ src/components/NewMovieForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import '../styles/project-styles.css';
+function NewMovieForm({ addMovie }) {
+  const [movie, setMovie] = useState({ name: '', image: '', rating: '' });
+
+  const handleChange = (event) => {
+    setMovie({ ...movie, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3001/movies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(movie)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Check if the movie was added successfully
+        if (data && data.id) {
+          // Call the addMovie function with the new movie data
+          addMovie(data);
+          // Reset the form
+          setMovie({ name: '', image: '', rating: '' });
+        }
+      })
+      .catch(error => console.log(error));
+  };
+  
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Name:
+        <input type="text" name="name" onChange={handleChange} value={movie.name} />
+      </label>
+      <label>Image URL:
+        <input type="text" name="image" onChange={handleChange} value={movie.image} />
+      </label>
+      <label>Rating:
+        <input type="text" name="rating" onChange={handleChange} value={movie.rating} />
+      </label>
+      <input type="submit" value="Add Movie" />
+    </form>
+  );
+}
+
+export default NewMovieForm;
