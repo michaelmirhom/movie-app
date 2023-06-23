00f3532e2f140b4bc9d4346b6be0e5ab1f0f2d67
michaelmirhom <micharby73@gmail.com>
2023-06-23 18:43:27 -0500

    Adding Css

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../styles/project-styles.css';
 
 function MoviesList({ movies, removeMovie }) {
   const movieItems = movies.map(movie => (
