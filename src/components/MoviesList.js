ec7fb5e034d25cee063ec7324eecee34b51eeb8a
michaelmirhom <micharby73@gmail.com>
2023-06-23 18:42:47 -0500

    Adding function MoviesList

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/project-styles.css';
+
+
+function MoviesList({ movies, removeMovie }) {
+  const movieItems = movies.map(movie => (
+    <div key={movie.id} className="movie-card">
+      <img src={movie.image} alt={movie.name} className="movie-image" />
+      <h3>{movie.name}</h3>
+      <p>Rating: {movie.rating}</p>
+      <Link to={`/movies/${movie.id}`} className="detail-button">Details</Link>  {/* Replace this line */}
+      <button onClick={() => removeMovie(movie.id)}>Remove</button>
+    </div>
+  ));
+
+  return (
+    <div className="movie-grid">
+      {movieItems}
+    </div>
+  );
+}
+
+export default MoviesList;
